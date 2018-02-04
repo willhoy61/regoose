75eabbff9afe0e0d97d39a74ba22ce19a57eb943
William Hoy <willhoy61@gmail.com>
2018-02-04 17:28:00 -0600

    stuff working

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+if(process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+}
+
+
+app.get("/test", function(req, res) {
+	res.json("all good");
+})
+
+app.get("*", function(req, res) {
+	res.sendFile(path.join(__dirname, "./client/build/index.html"));
+})
+
+
+app.listen(PORT, function() {
+	console.log(`Serving on port ${PORT}`);
+})
